a2b3fefd745f0d1a75c40fc1bf7495a110028a46
Abhishek <alphasaurs@gmail.com>
2022-03-22 21:55:04 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+   freopen("inputf.in", "r", stdin);
+   freopen("outputf.in", "w", stdout);
+
+#endif
+
+
+   cout << "hello";
+
+
+   return 0;
+
+}
+
+
+
